153a753a559522d34e4d88d42ed3c0d4f17aac78
GHULAM MUSTAFA <161878014+Mustafa-Khan786@users.noreply.github.com>
2024-07-07 11:41:41 -0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,33 +1,23 @@
+mport inquirer from 'inquirer';
+type ansType = {
+    userGuess: number 
+}
+const syestemGeneratedNo = Math.floor(Math.random()  * 10);
 
-import inquirer from "inquirer";
-const answers =await inquirer.prompt([
-{
-    type:"number",
-    name:"numone",
-    message: "kindly inter your first number",
-
-}, 
-{type:"number",
-    name:"numtwo",
-     message: "kindly inter your second number",
-
-},
-{
-    type: "list",
-    name:"opperator",
-    choices: ['addition','subtraction','multiplication',"division"],
-    message:"Sellect your opperator:"
-}
-]) 
- if (answers.opperator ==="addition") {
-    console.log(answers.numone + answers.numtwo)   
-}
-if (answers.opperator ==="subtraction") {
-    console.log(answers.numone - answers.numtwo)  
-}
-if (answers.opperator ==="multiplication") {
-    console.log(answers.numone * answers.numtwo)
-}
-if (answers.opperator ==="division") {
-            console.log(answers.numone / answers.numtwo)   
+const answer : ansType = await inquirer.prompt([
+    {
+        type: "number",
+        name:"userguess",
+        message: "write your guess number b/w 1 to 10:"
     }
+])
+console.log( answer.userGuess);
+
+
+const {userGuess} = answer;
+console.log(userGuess,"userGuess",syestemGeneratedNo,'SYs')
+if (userGuess === syestemGeneratedNo){
+    console.log("yeaa your answer is correct \n you win!")
+}else {
+    console.log("please try again better luck next time!")
+}
